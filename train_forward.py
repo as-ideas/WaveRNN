eab2cba1c33d616c5128fe07a1f8ce6045f3cfc7
cschafer26 <c.schaefer.home@gmail.com>
2022-09-19 12:36:10 +0200

    Impl fast pitch.

--- train_forward.py
+++ train_forward.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     print(f'\nInitialized tts model: {model}\n')
 
     print('Loading semb')
-    sembs = list(paths.speaker_emb.glob('*.npy'))[:10]
+    sembs = list(paths.speaker_emb.glob('*.npy'))
 
     print(f'Speaker names: {speaker_names}')
     speaker_emb = {name: np.zeros(256) for name in speaker_names}
