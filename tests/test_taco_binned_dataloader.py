180a4f6138021abcb50293e01636fd017ad4d30d
cschafer26 <c.schaefer.home@gmail.com>
2023-01-10 15:32:07 +0100

    Fix remaining tests.

--- tests/test_taco_binned_dataloader.py
+++ tests/test_taco_binned_dataloader.py
@@ -20,7 +20,10 @@ class TestTacoBinnedDataloader(unittest.TestCase):
         self.paths = Paths(data_path=self.temp_dir.name + '/data', tts_id='tts_test_id')
         self.dataset = [('id_1', 2), ('id_2', 2), ('id_3', 3), ('id_4', 4), ('id_5', 4), ('id_6', 4)]
         self.text_dict = {'id_1': 'aa', 'id_2': 'aa', 'id_3': 'aaa', 'id_4': 'aaaa', 'id_5': 'aaaa', 'id_6': 'aaaa'}
+        self.speaker_dict = {'id_1': 'speaker_1', 'id_2': 'speaker_2', 'id_3': 'speaker_3', 'id_4': 'speaker_4',
+                             'id_5': 'speaker_5', 'id_6': 'speaker_6'}
         pickle_binary(self.text_dict, self.paths.data / 'text_dict.pkl')
+        pickle_binary(self.speaker_dict, self.paths.data / 'speaker_dict.pkl')
         for id, mel_len in self.dataset:
             np.save(self.paths.mel / f'{id}.npy', np.ones((5, mel_len)), allow_pickle=False)
             np.save(self.paths.speaker_emb / f'{id}.npy', np.ones((1, mel_len)), allow_pickle=False)
