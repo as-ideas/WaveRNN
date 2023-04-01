8b08ef58cd103ce1959b848bf24e484bb1497788
cschafer26 <c.schaefer.home@gmail.com>
2023-04-01 10:25:06 +0200

    Some fixes.

--- train_char_transformer.py
+++ train_char_transformer.py
@@ -1,6 +1,7 @@
 import math
 import random
 from random import Random
+from typing import Tuple
 
 import pandas as pd
 # Define the dataset
@@ -84,9 +85,11 @@ class TransformerModel(nn.Module):
         Returns:
             output Tensor of shape [seq_len, batch_size, ntoken]
         """
+        src_pad = make_token_len_mask(src)
         src = self.encoder(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, None)
+
+        output = self.transformer_encoder(src, src_key_padding_mask=src_pad)
         output = self.decoder(output)
         return output
 
@@ -95,34 +98,48 @@ def generate_square_subsequent_mask(sz: int) -> Tensor:
     """Generates an upper-triangular matrix of -inf, with zeros on diag."""
     return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
 
+def make_token_len_mask(x: torch.Tensor) -> torch.Tensor:
+    return (x == 0).transpose(0, 1)
+
+def mask_tensor(x: torch.Tensor, probs: Tuple[float, float, float]) -> tuple:
+    a, b, c = probs
+    rand_mask = torch.rand(x.size()).to(x.device)
+    rand_inds = torch.randint(low=1, high=len(phonemes), size=x.size()).to(x.device)
+    x[rand_mask < a - c] = mask_index
+    x[rand_mask < b] = rand_inds[rand_mask < b]
+    return x, rand_mask < a
+
+
+
+
 
 if __name__ == '__main__':
 
     df = pd.read_csv('/Users/cschaefe/datasets/nlp/welt_articles_phonemes.tsv', sep='\t', encoding='utf-8')
     df.dropna(inplace=True)
     strings = df['phonemes']
-    strings = [s for s in strings if len(s) > 10]
+    strings = [s for s in strings if len(s) > 10 and len(s) < 300]
     random = Random(42)
     random.shuffle(strings)
 
     print(strings[:10])
 
-    dataset = StringDataset(strings[100:])
-    val_dataset = StringDataset(strings[:100])
-    dataloader = DataLoader(dataset, batch_size=2, collate_fn=collate_fn)
-    val_dataloader = DataLoader(val_dataset, batch_size=2, collate_fn=collate_fn)
+    dataset = StringDataset(strings[512:])
+    val_dataset = StringDataset(strings[:512])
+    dataloader = DataLoader(dataset, batch_size=32, collate_fn=collate_fn)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, collate_fn=collate_fn)
 
-    device = torch.device('cpu')#torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('mps')#torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     ntokens = len(phonemes) + 1  # size of vocabulary
     mask_index = len(phonemes)
     emsize = 512  # embedding dimension
     d_hid = 512  # dimension of the feedforward network model in nn.TransformerEncoder
-    nlayers = 2  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-    nhead = 4  # number of heads in nn.MultiheadAttention
-    dropout = 0.1  # dropout probability
+    nlayers = 4  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+    nhead = 8  # number of heads in nn.MultiheadAttention
+    dropout = 0.  # dropout probability
     model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
 
-    criterion = nn.CrossEntropyLoss(ignore_index=0).to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=0, reduction='none').to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
     model.train()  # turn on train mode
@@ -130,26 +147,34 @@ if __name__ == '__main__':
     log_interval = 200
     step = 0
 
+    mask_probs = 0.3, 0.03, 0.03
+
     eval_sent = 'diː t͡seː-deː-ʔuː-t͡sɛntʁaːlə lɛst iːɐ ʃpɪt͡sn̩pɛʁzonaːl dʊʁçt͡ʃɛkn̩: ɪm jʏŋstn̩ mɪtɡliːdɐbʁiːf fɔn bʊndəsɡəʃɛft͡sfyːʁɐ ʃtɛfan hɛnəvɪç (axtʔʊntfɪʁt͡sɪç) vɪʁt diː t͡seː-deː-ʔuː-baːzɪs aʊfɡəfɔʁdɐt, an aɪnɐ bəfʁaːɡʊŋ dɛs tʁiːʁɐ paʁtaɪən-fɔʁʃɐs uːvə jan (nɔɪnʔʊntfʏnft͡sɪç) taɪlt͡suneːmən'
     eval_input = Tokenizer()(eval_sent)
     eval_tens = torch.tensor(eval_input).unsqueeze(0).to(device)
-    eval_mask = (torch.rand(eval_tens.size()) < 0.2).to(device)
-    eval_tens[eval_mask] = mask_index
+    eval_tens, eval_mask = mask_tensor(eval_tens, mask_probs)
+    #eval_mask = (torch.rand(eval_tens.size()) < 0.3).to(device)
+    #eval_tens[eval_mask] = mask_index
 
     sw = SummaryWriter(log_dir='checkpoints/lm_summary')
+
+
 
     for epoch in range(100):
         for batch in tqdm.tqdm(dataloader, total=len(dataloader)):
             batch = batch.to(device)
             batch_target = torch.clone(batch.detach())
-            rand_mask = (torch.rand(batch.size()) < 0.2).to(device)
-            batch[rand_mask] = mask_index
+            batch, rand_mask = mask_tensor(batch, mask_probs)
+            #rand_mask = (torch.rand(batch.size()) < 0.3).to(device)
+            #batch[rand_mask] = mask_index
+            batch_target[~rand_mask] = 0
             output = model(batch)
             loss = criterion(output.transpose(1, 2), batch_target)
+            loss = loss.sum() / (rand_mask.sum() + 1e-10)
 
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
             optimizer.step()
             step += 1
             sw.add_scalar('masked_loss', loss, global_step=step)
@@ -159,16 +184,18 @@ if __name__ == '__main__':
                 for batch in tqdm.tqdm(val_dataloader, total=len(dataloader)):
                     batch = batch.to(device)
                     batch_target = torch.clone(batch.detach())
-                    rand_mask = (torch.rand(batch.size()) < 0.2).to(device)
+                    rand_mask = (torch.rand(batch.size()) < 0.3).to(device)
                     batch[rand_mask] = mask_index
+                    batch_target[~rand_mask] = 0
                     with torch.no_grad():
                         output = model(batch)
                         loss = criterion(output.transpose(1, 2), batch_target)
+                        loss = loss.sum() / (rand_mask.sum() + 1e-10)
                         val_loss += loss
                         val_norm += 1
 
                 with torch.no_grad():
-                    out = model(eval_tens)
+                    out = model(eval_tens).cpu()
                     out = torch.argmax(out[0], dim=-1)
                     print(out)
                     out_text = Tokenizer().decode(out.tolist())
@@ -179,7 +206,7 @@ if __name__ == '__main__':
                         if a == b:
                             eval_score += 1
                     sw.add_scalar('eval/score', eval_score / len(eval_sent), global_step=step)
-                    sw.add_scalar('eval/loss', val_loss / val_norm / len(eval_sent), global_step=step)
+                    sw.add_scalar('eval/loss', val_loss / val_norm, global_step=step)
                     print(step, loss, eval_score / len(eval_sent))
                     print(eval_sent)
                     print(out_text)
