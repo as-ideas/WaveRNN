d02f0fdb9c37267e0656a3abb09f825f73047d72
cschafer26 <c.schaefer.home@gmail.com>
2023-08-02 09:29:28 +0200

    Impl

--- process_pos.py
+++ process_pos.py
@@ -1,11 +1,43 @@
-from dp.phonemizer import Phonemizer
-
-
-
-
+import pandas as pd
+import numpy as np
+from utils.files import pickle_binary
 
 if __name__ == '__main__':
+    df = pd.read_csv('/Users/cschaefe/datasets/nlp/bild_phons_pos.tsv', sep='\t', encoding='utf-8')
 
-    text = 'Verteidigungsminister Boris Pistorius (63, SPD) treibt die Beschaffung voran.'
-    phonemizer = Phonemizer.from_checkpoint('/Users/cschaefe/workspace/tts-synthv3/app/11111111/models/bild_voice/phon_model/model.pt')
+    all_pos = set()
+
+    for pos in df['pos']:
+        all_pos.update(set(pos.split('|')))
+
+    all_pos = sorted(list(all_pos))
+
+    pos_num = {p: i for i, p in enumerate(all_pos, 2)}
+    print(pos_num)
+
+    text_dict = {}
+    pos_dict = {}
+
+    for id, phons, pos in zip(df['file_id'], df['text_phonemized'], df['pos']):
+        phons = phons.split()
+        pos = pos.split('|')
+
+        pos_ids = []
+        for p, pp in zip(phons, pos):
+            ids = pos_num[pp]
+            pos_ids.extend([ids]*len(p) + [1])
+
+        phons = ' '.join(phons)
+        pos_ids = pos_ids[:len(phons)]
+        pos_ids = np.array(pos_ids)
+
+        text_dict[id] = phons
+
+
+        print(phons)
+        print(pos_ids)
+
+        np.save(f'/Users/cschaefe/datasets/nlp/pos/phon_pos/{id}.npy', pos_ids)
+    pickle_binary(text_dict, '/Users/cschaefe/datasets/nlp/pos/text_dict_bild_pos.pkl')
+    pickle_binary(pos_num, '/Users/cschaefe/datasets/nlp/pos/pos_dict.pkl')
 
