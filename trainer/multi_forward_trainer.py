e9deed8057a42f7344108e5fd2003363268c1742
cschafer26 <c.schaefer.home@gmail.com>
2023-09-19 09:47:07 +0200

    Fix.

--- trainer/multi_forward_trainer.py
+++ trainer/multi_forward_trainer.py
@@ -271,12 +271,14 @@ class MultiForwardTrainer:
                     for feat_fake_i, feat_real_i in zip(feat_fake, feat_real):
                         g_loss += 10. * F.l1_loss(feat_fake_i, feat_real_i.detach())
 
+                norm, spec = multires_stft_loss(wav_fake.squeeze(1), batch['wav'])
+
                 loss = m1_loss + m2_loss \
                        + self.train_cfg['dur_loss_factor'] * dur_loss \
                        + self.train_cfg['pitch_loss_factor'] * pitch_loss \
                        + self.train_cfg['energy_loss_factor'] * energy_loss \
                        + self.train_cfg['pitch_cond_loss_factor'] * pitch_cond_loss \
-                       + 0.1 * g_loss
+                       + 0.1 * g_loss + norm + spec
 
                 pitch_cond_true_pos = (torch.argmax(pred['pitch_cond'], dim=-1) == batch['pitch_cond'])
                 pitch_cond_acc = pitch_cond_true_pos[batch['pitch_cond'] != 0].sum() / (batch['pitch_cond'] != 0).sum()
@@ -311,6 +313,8 @@ class MultiForwardTrainer:
                 if step % self.train_cfg['plot_every'] == 0:
                     self.generate_plots(model, session, g_model, torch_stft)
 
+                self.writer.add_scalar('norm_loss/train', norm, model.get_step())
+                self.writer.add_scalar('spec_loss/train', spec, model.get_step())
                 self.writer.add_scalar('g_loss/train', g_loss, model.get_step())
                 self.writer.add_scalar('d_loss/train', d_loss, model.get_step())
                 self.writer.add_scalar('Mel_Loss/train', m1_loss + m2_loss, model.get_step())
