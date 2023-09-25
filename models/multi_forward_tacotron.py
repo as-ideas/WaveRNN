d2575dca6a84722675d23d6facfa7ddc34e3f5b5
cschafer26 <c.schaefer.home@gmail.com>
2023-09-25 10:59:26 +0200

    Fix.

--- models/multi_forward_tacotron.py
+++ models/multi_forward_tacotron.py
@@ -18,13 +18,12 @@ class Discriminator(nn.Module):
         self.embedding = Embedding(len(phonemes), 64)
         self.pitch_cond_embedding = Embedding(4, 8)
         self.convs = torch.nn.ModuleList([
-            BatchNormConv(64 + 256 + 8, 256, 3, relu=True),
+            BatchNormConv(64 + 256 + 8 + 2, 256, 3, relu=True),
             BatchNormConv(256, 256, 3, relu=True),
             BatchNormConv(256, 256, 3, relu=True),
         ])
-        self.gru1 = nn.GRU(256, 64, bidirectional=True)
-        self.gru2 = nn.GRU(2, 64, bidirectional=True)
-        self.lin = nn.Linear(256, 1)
+        self.gru = nn.GRU(256, 64, bidirectional=True)
+        self.lin = nn.Linear(128, 1)
 
     def forward(self, x, dur, pitch, semb, x_cond):
         emb = self.embedding(x)
@@ -32,15 +31,13 @@ class Discriminator(nn.Module):
         x_cond = self.pitch_cond_embedding(x_cond)
         speaker_emb = semb[:, None, :]
         speaker_emb = speaker_emb.repeat(1, x.shape[1], 1)
-        x = torch.cat([emb, speaker_emb, x_cond], dim=2)
+        x = torch.cat([emb, speaker_emb, x_cond, dur, pitch], dim=-1)
         x = x.transpose(1, 2)
         for conv in self.convs:
             x = conv(x)
+            x = F.dropout(x, training=self.training, p=0.5)
         x = x.transpose(1, 2)
-        x1, _ = self.gru1(x)
-        x2 = torch.cat([dur, pitch], dim=-1)
-        x2, _ = self.gru2(x2)
-        x = torch.cat([x1, x2], dim=-1)
+        x, _ = self.gru(x)
         x = self.lin(x)
         return x
 
